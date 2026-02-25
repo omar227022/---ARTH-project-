cfd1358633aa7db0e4dd7662b353f69f586c4118
Omar (ARTH) ارث <omargg229022@gmail.com>
2026-02-25 06:31:47 +0300

    .

--- dotconfig/theme-hack/change-bg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-IMG=$(find ~/Pictures/Wallpapers ~/Downloads/Images ~/Desktop/Backgrounds -type f \( -name "*.jpg" -o -name "*.png" \) | shuf -n 1)
-wal -i "$IMG"
-pkill waybar; waybar -c ~/.config/theme-hack/config.jsonc -s ~/.config/theme-hack/style.css &
-swaync-client -t -swb
